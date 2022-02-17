0a3295450e342afa3a5430e3f667b131babde9cd
rrodriguezcs <90003077+rrodriguezcs@users.noreply.github.com>
2022-02-16 21:26:49 -0800

    education

--- index.php
+++ index.php
@@ -40,11 +40,18 @@
       </div>
       <hr>
       <div id="education" class="content-item">
-        <div class="vertical-center">
-          <h2>Education</h2>
-          <p>...</p>
-        </div>
-      </div>
+      <?php foreach($json_data['education'] AS $education) { ?>
+  <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+    <div class="flex-grow-1">
+      <h3 class="mb-0"><?php echo $education['title']; ?></h3>
+      <div class="subheading mb-3"><?php echo $education['education']; ?></div>
+      <p><?php echo $education['description']; ?></p>
+    </div>
+    <div class="flex-shrink-0">
+      <span class="text-primary"><?php echo $experience['period']; ?></span>
+    </div>
+  </div>
+      <?php  } ?>
       <hr>
       <div id="experience" class="content-item">
       <?php foreach($json_data['experience'] AS $experience) { ?>
